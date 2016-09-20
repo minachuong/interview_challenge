db11b6d5fc8809eca9c69d6179471808ec3ebaa7
Mina Chuong <mina.chuong@gmail.com>
2016-09-20 15:07:25 -0700

    ng-repeat for detailed product info

--- js/app.js
+++ js/app.js
@@ -15,11 +15,11 @@
     'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
                     'src'        : 'material/000010885.jpg'
   };
-  $scope.products = {
-    1: {
+  $scope.products = [
+    {
         id : 1,
         order: 1,
-        title : 'Product Title 1',
+        'title' : 'Product Title 1',
         'price' : 'Staring at $500',
         'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.<br><br>'
             +'<ul>'
@@ -30,7 +30,7 @@
         'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
                         'src'        : 'material/000010885.jpg'
     },
-    2: {
+    {
         id : 2,
         order: 2,
         'title' : 'Product Title 2',
@@ -39,7 +39,7 @@
         'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
                         'src'        : 'material/ff_x20_008.JPG'
     },
-    3: {
+    {
         id : 3,
         order: 3,
         'title' : 'Product Title 3',
@@ -48,15 +48,15 @@
         'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
                         'src'        : 'material/sample1.jpg'
     },
-    4: {
+    {
         id : 4,
         order: 4,
-        'title' : 'Product Title 3',
+        'title' : 'Product Title 4',
         'price' : 'Staring at $731',
         'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.',
         'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
                         'src'        : 'material/sample_08.jpg'
     }
-  };
+  ];
 });
 })();
